62b673379f80317376a919b7dda891472bb32999
Theyme <themiladeyopaleke@gmail.com>
2020-01-06 14:25:30 -0500

    ball is life

---
+++ src/test/java/com/example/demo/Exercise02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
